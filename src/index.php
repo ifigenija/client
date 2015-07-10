b8198aecb39528834913159e6ff5e14f44570bf5
Boris Lasic <boris@max.si>
2015-07-10 08:34:38 +0200

    index fajli za zagon

---
+++ src/index.php
@@ -0,0 +1,37 @@
+<?php
+$config = include 'config.php';
+$dist = $config['useDist'] ? 'dist/': '';
+?><!DOCTYPE html>
+<html>
+    <head>
+        <title>Ifigenija</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     
+            <link rel="stylesheet" href="<?= $dist ?>css/public.css">
+            <link rel="stylesheet" href="<?= $dist ?>css/layout.css">
+            <link rel="stylesheet" href="<?= $dist ?>css/backgrid.css">
+            <link rel="stylesheet" href="<?= $dist ?>css/site.css">        
+        <link rel="stylesheet" href="lib/font-awsome/css/font-awesome.css">
+        <script type="text/javascript" src="lib/requirejs/require.js"></script>        
+    </head>
+    <body>
+        <div class="container">
+            <nav id="nav" class="navbar navbar-default"></nav>
+            <div id="glavni-container">
+
+            </div>
+        </div>        
+        <script type="text/javascript">
+            define('baseUrl', function () {
+                return "/";
+            });
+            require(['<?= $config['useDist'] ? 'requireconfig' : 'requiredist' ?>'], function () {
+                require(['app/public', 'backbone'], function (public, Backbone) {
+                    public.start();
+                    Backbone.history.start();
+                });
+            });
+        </script>
+    </body>
+</html>
---
+++ src/main.php
@@ -0,0 +1,32 @@
+<?php
+$config = include 'config.php';
+$dist = $config['useDist'] ? 'dist/': '';
+?><!DOCTYPE html>
+<html>
+    <head>
+        <title>Ifigenija</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="<?= $dist ?>css/yeti.css">
+        <link rel="stylesheet" href="<?= $dist ?>css/layout.css">
+        <link rel="stylesheet" href="<?= $dist ?>css/backgrid.css">
+        <link rel="stylesheet" href="<?= $dist ?>css/site.css">
+        <link rel="stylesheet" href="lib/font-awsome/css/font-awesome.css">
+        <script type="text/javascript" src="lib/requirejs/require.js"></script>   
+        <script type="text/javascript">
+            define('baseUrl', function () {
+                return "";
+            });
+            require(['<?= $config['useDist'] ? 'requireconfig' : 'requiredist' ?>'], function () {
+                require(['app/main', 'backbone', 'jquery', 'app/aaa/checkauth'], function (app, Backbone, $, auth) {
+                    auth(function (user) {
+                        app.start({user: user});
+                        Backbone.history.start();
+                    });
+                });
+            });
+        </script>
+    </head>
+    <body>
+    </body>
+</html>
