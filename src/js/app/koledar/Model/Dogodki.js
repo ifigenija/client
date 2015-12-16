1b221bff15296bfff4e39612549c7a9eba8da221
Lovro Rojko <lovro.rojko@gmail.com>
2015-12-16 15:22:59 +0100

    PlanerCollection dogodki pretvori

--- src/js/app/koledar/Model/Dogodki.js
+++ src/js/app/koledar/Model/Dogodki.js
@@ -23,7 +23,7 @@ define(['baseUrl', 'backbone', 'app/Max/Model/MaxPageableCollection', 'underscor
             eObj.end = moment(this.get('konec'));
             return eObj;
         },
-        initialize: function(attr){
+        initialize: function (attr) {
             this.view = attr.view || this.view;
         }
     });
@@ -33,7 +33,7 @@ define(['baseUrl', 'backbone', 'app/Max/Model/MaxPageableCollection', 'underscor
         start: null, // hrani datum, od kdaj smo nalagali dogodke
         end: null, // hrani čas do kdaj smo nalagali dogodke
         model: Dogodek,
-        mode: "server",
+        mode: 'server',
         state: {
             pageSize: 1000,
             currentPage: 1
@@ -42,9 +42,44 @@ define(['baseUrl', 'backbone', 'app/Max/Model/MaxPageableCollection', 'underscor
         url: function () {
             return baseUrl + '/rest/dogodek/' + this.view;
         }
-
-
     });
+
+    Dogodki.prototype.pretvori = function () {
+
+        this.comparator = 'zacetek';
+        this.sort();
+        var collection = new PlanerCollection();
+
+        for (var id in this.models) {
+            var model = this.models[id];
+            var planerModel = new PlanerCollection.prototype.model();
+
+            var zacetek = moment(model.get('zacetek'));
+            var konec = moment(model.get('konec'));
+            
+            planerModel.datum = moment(zacetek, "DD.MM.YYYY");
+
+            var dopoldan = moment('14:00', 'HH:mm');
+            var popoldan = moment('19:00', 'HH:mm');
+            var zvecer = moment('23:00', 'HH:mm');
+
+            if (dopoldan.diff(zacetek, 'minutes') > 0) {
+                planerModel.dopoldanColl.add(model);
+            } else if (popoldan.diff(zacetek, 'minutes') > 0) {
+                planerModel.popoldanColl.add(model);
+            } else if (zvecer.diff(zacetek, 'minutes') > 0) {
+                planerModel.zvecerColl.add(model);
+            }
+            
+            if (dopoldan.diff(konec, 'minutes') > 0) {
+                planerModel.dopoldanColl.add(model);
+            } else if (popoldan.diff(konec, 'minutes') > 0) {
+                planerModel.popoldanColl.add(model);
+            } else if (zvecer.diff(konec, 'minutes') > 0) {
+                planerModel.zvecerColl.add(model);
+            }
+        }
+    };
 
     return Dogodki;
 
