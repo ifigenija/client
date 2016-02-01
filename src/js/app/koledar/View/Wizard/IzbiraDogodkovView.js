cf4459c7101168711c845ff4f271b9cdaab598d1
Lovro Rojko <lovro.rojko@gmail.com>
2016-02-01 10:48:34 +0100

    IzbiraDogodkovView

---
+++ src/js/app/koledar/View/Wizard/IzbiraDogodkovView.js
@@ -0,0 +1,121 @@
+/* 
+ * Licenca GPLv3
+ */
+define([
+    'radio',
+    'i18next',
+    'app/bars',
+    'underscore',
+    'backbone',
+    'marionette',
+    '../../Model/Dogodki',
+    'backgrid',
+    'backgrid-select-all'
+], function (
+        Radio,
+        i18next,
+        Handlebars,
+        _,
+        Backbone,
+        Marionette,
+        Dogodki,
+        Backgrid
+        ) {
+    
+    var termini = {};
+
+    var columns = [
+        {
+            cell: 'select-row',
+            headerCell: 'select-all',
+            name: ""
+        },
+        {
+            cell: 'string',
+            editable: false,
+            label: i18next.t('dogodek.title'),
+            name: 'title',
+            sortable: true
+        },
+        {
+            headerCell: 'number',
+            cell: 'date',
+            editable: false,
+            label: i18next.t('dogodek.zacetek'),
+            name: 'zacetek',
+            sortable: true
+        },
+        {
+            headerCell: 'number',
+            cell: 'date',
+            editable: false,
+            label: i18next.t('dogodek.konec'),
+            name: 'zacetek',
+            sortable: true
+        },
+        {
+            cell: 'string',
+            editable: false,
+            label: i18next.t('dogodek.razred'),
+            name: 'razred',
+            sortable: true
+        }
+    ];
+
+    var IzbiraDogodkovView = Marionette.LayoutView.extend({
+        clasName: 'Izbira-dogodkov',
+        template: Handlebars.compile('<div class="region-seznam-dogodki"></div>'),
+        regions: {
+            seznamR: '.region-seznam-dogodki'
+        }
+    });
+
+    IzbiraDogodkovView.prototype.initialize = function (options) {
+        this.dogodki = new Dogodki();
+        if (options && options.model) {
+            //želimo pridobiti dogodke, ki se začnejo v času gosto vanja
+            this.model = options.model;
+            this.dogodki.queryParams.zacetek = this.model.get('zacetek');
+            this.dogodki.queryParams.konec = this.model.get('konec');
+
+            this.dogodki.fetch({
+                error: Radio.channel('error').request('handler', 'xhr')
+            });
+        }
+    };
+
+    IzbiraDogodkovView.prototype.onRender = function () {
+        this.gridView = new Backgrid.Grid({
+            columns: columns,
+            collection: this.dogodki
+        });
+
+        this.dogodki.on('backgrid:selected', this.selected, this);
+
+        this.seznamR.show(this.gridView);
+    };
+
+    IzbiraDogodkovView.prototype.selected = function () {
+        var models = this.gridView.getSelectedModels();
+        if (models.length) {
+            var ids = [];
+            ids = _.pluck(models, 'id');
+
+            var osebe = [];
+            _.each(models, function (model) {
+                var ts = model.get('terminiStoritve');
+                _.each(ts, function (tsModel) {
+                    osebe.push(tsModel.id);
+                });
+            });
+            
+            this.model.set('dogodki', ids);
+            this.model.set('sodelujoci', osebe);
+            this.trigger('ready', this.model);
+        } else {
+            this.trigger('not:ready', this.model);
+        }
+    };
+
+    return IzbiraDogodkovView;
+});
